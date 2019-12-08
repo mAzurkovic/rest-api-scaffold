35e3bce3fcf126456c372ce1e3f50616e8be020f
Mattias Zurkovic <mattias@zurkovic.com>
2019-12-07 16:33:25 -0800

    add read items

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,21 @@ var router = express.Router();
 const Item = require('../models/item');
 
 router.get('/', function(req, res, next) {
-  res.sendStatus(200);
+  var nameQuery = req.query.name;
+
+  // Return all if no query specified
+  if (typeof nameQuery == 'undefined') {
+    Item.find(function(err, items) {
+      if (err) return console.error(err);
+      res.send(items);
+    });
+  } else {
+  // Return by item name
+    Item.find({name: nameQuery}, function(err, items) {
+      if (err) return console.error(err);
+      res.send(items);
+    });
+  }
 });
 
 router.post('/:name', function(req, res) {
@@ -15,7 +29,7 @@ router.post('/:name', function(req, res) {
 
   // Use mongoose to save it in db
   item.save(function(err) {
-    if (err) { return next(err); }
+    if (err) return next(err);
     res.sendStatus(200);
   });
   
